4f6aef8b7ae17f5a732f859f681069b22270575d
GiraffeLars <203875086+GiraffeLars@users.noreply.github.com>
2026-08-02 22:24:36 +0200

    Add Theme.cs along with possible colors for both light and dark themes

---
+++ src/Picross.Maui/Theme.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picross.Maui
+{
+    internal class Theme
+    {
+        private static bool IsDarkMode()
+        {
+            AppTheme? systemTheme = Application.Current?.RequestedTheme;
+            return systemTheme == AppTheme.Dark;
+        }
+
+        public static Color FilledCell => IsDarkMode() ? Colors.White : Colors.Black;
+        public static Color SolvedCell => Colors.Green;
+        public static Color GridLine => IsDarkMode() ? Colors.LightGray : Colors.Gray;
+        public static Color IncompleteHint => IsDarkMode() ? Colors.White : Colors.Black;
+        public static Color CompletedHint => IsDarkMode() ? Colors.LightGray : Colors.Gray;
+        public static Color Background => IsDarkMode() ? Colors.Black : Colors.White;
+    }
+}
