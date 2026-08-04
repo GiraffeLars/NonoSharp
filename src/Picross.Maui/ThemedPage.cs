775ae48e6412615073d3d8afcd6d444b25536991
GiraffeLars <203875086+GiraffeLars@users.noreply.github.com>
2026-08-04 16:55:47 +0200

    Add views dictionary to ThemedPage and reworked GamePage appropiately

--- src/Picross.Maui/ThemedPage.cs
+++ src/Picross.Maui/ThemedPage.cs
@@ -5,18 +5,30 @@ using System.Text;
 namespace Picross.Maui
 {
     /// <summary>
-    /// A <c>ContentPage</c> that automatically changes themes when the system's theme changes
+    /// A <c>ContentPage</c> that automatically changes themes when the system's theme changes.
+    /// Supports add views to <c>this.views</c> to be automatically invalidated when the theme changes as well
     /// </summary>
     public partial class ThemedPage : ContentPage
     {
+        protected Dictionary<string, GraphicsView> views;
         public ThemedPage() : base() 
         {
+            views = new();
             // Set Background color (in case of system mismatch) and add event handling when system theme changes
             BackgroundColor = Theme.BackgroundColor;
             Application.Current!.RequestedThemeChanged += (s, a) =>
             {
                 BackgroundColor = Theme.BackgroundColor;
+                this.InvalidateViews();
             };
+        }
+
+        internal void InvalidateViews()
+        {
+            foreach(GraphicsView view in views.Values)
+            {
+                view.Invalidate();
+            }
         }
     }
 }
