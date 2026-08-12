9bd27f9a09d655b99b01b64389a8ed3e81b04918
GiraffeLars <203875086+GiraffeLars@users.noreply.github.com>
2026-08-12 19:21:46 +0200

    Saving and loading pre-made puzzles (#19)
    * Add basic puzzle definition and first iteration of serialization and deserialization
    * Add tests for Serialize, Deserialize and methods they use
    * Load and save support in API
    * Asynchronous methods for saving and loading

---
+++ src/Picross.Game/PuzzleDefinition.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Picross.Game
+{
+    internal class PuzzleDefinition
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        /// <summary>
+        /// The solution in bool, going from left-to-right, top-to-bottom.
+        /// </summary>
+        public bool[] Solution { get; private set; }
+        // Bools take 1 bit, ints 32. Depending on the density and dimensions of the puzzle, one can be more optimal for storage space
+        // but considering that usually >50% of cells are filled, storing the expected state of each cell is very defendable
+
+        public static int Version { get; } = 0;
+
+        /// <summary>
+        /// Total amount of bytes used by solution, rounded up
+        /// </summary>
+        private int SolutionLengthBytes => (int)Math.Ceiling((double)Solution.Length / 8);
+
+        internal PuzzleDefinition(int width, int height, bool[] solution)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.Solution = solution;
+        }
+
+        internal PuzzleDefinition(int width, int height, List<Point> solution) : 
+            this(width, height, ConvertPointSolutionToBools(width, height, solution)) { }
+
+        public static bool[] ConvertPointSolutionToBools(int width, int height, List<Point> solution)
+        {
+            bool[] boolSol = new bool[width * height];
+
+            foreach (Point p in solution)
+            {
+                // Set the corresponding location of the point in boolSol to true
+                int loc = p.X + p.Y * width;
+                boolSol[loc] = true;
+            }
+
+            return boolSol;
+        }
+
+        internal List<Point> ConvertBoolSolutionToPoints()
+        { 
+            List<Point> points = new List<Point>();
+            for (int i = 0; i < Solution.Length; i++)
+            {
+                if (Solution[i])
+                {
+                    int x = i % Width;
+                    int y = i / Height;
+                    points.Add(new(x, y));
+                }
+            }
+            return points;
+        }
+
+        public byte[] Serialize()
+        {
+            // 3 for width, height and version (ints are 32 bits = 4 bytes), and size of boolean solution in bytes, rounded up
+            int expectedCapacity = 3 * 4 + SolutionLengthBytes;
+            MemoryStream ms = new();
+
+            BinaryWriter bw = new BinaryWriter(ms);
+            bw.Write(Version);
+            bw.Write(Width);
+            bw.Write(Height);
+
+            bw.Write(ConvertSolutionToBytes());
+
+            bw.Close();
+            ms.Close();
+            return ms.ToArray();
+        }
+
+        internal byte[] ConvertSolutionToBytes()
+        {
+            byte[] bytes = new byte[SolutionLengthBytes];
+
+            for (int i = 0; i < Width * Height; i++)
+            {
+                // Append the solution bit to the byte of its location
+                // Both arrays read from left to right, as done by the << operator and 7 - i%8, resulting in the first
+                // character being the leading character in the byte as well, etc.
+
+                // Check if the solution is true, otherwise skip this appending step, as it is already 0 by default
+                if (Solution[i])
+                {
+                    bytes[i / 8] |= (byte) (1 << 7 - i % 8);
+                }
+            }
+            return bytes;
+        }
+
+        public static PuzzleDefinition Deserialize(byte[] serializedPuzzle)
+        {
+            MemoryStream ms = new(serializedPuzzle);
+            BinaryReader br = new BinaryReader(ms);
+
+
+            int readVersion = br.ReadInt32();
+            if (readVersion != Version)
+            {
+                throw new NotSupportedException(
+                    $"The given puzzle's version ({readVersion}) does not match the current version ({Version})!");
+            }
+
+            int readWidth = br.ReadInt32();
+            int readHeight = br.ReadInt32();
+
+            byte[] remaining = br.ReadBytes((int)Math.Ceiling((double)(readWidth * readHeight) / 8));
+            bool[] readSolution = ConvertBytesToSolution(remaining, readWidth, readHeight);
+
+            return new(readWidth, readHeight, readSolution);
+        }
+
+        internal static bool[] ConvertBytesToSolution(byte[] bytes, int width, int height)
+        {
+            bool[] sol = new bool[width * height];
+
+            for (int i = 0; i < sol.Length; i++)
+            {
+                byte b = bytes[i / 8];
+
+                // Check if bit at corresponding position is set (this num is 0 if they do not match, otherwise, this is 2^x)
+                if ((b & (byte)(1 << 7 - i % 8)) != 0) {
+                    sol[i] = true;
+                }
+            }
+            return sol;
+        }
+
+        /// <summary>
+        /// Saves this puzzle at <paramref name="path"/>. Specifically, the expected solution and dimension are stored.
+        /// See also <seealso cref="File.WriteAllBytes(string, byte[])"/>.
+        /// </summary>
+        /// <param name="path">The path to save the puzzle to</param>
+        public void SavePuzzle(string path)
+        {
+            byte[] serialized = Serialize();
+            File.WriteAllBytes(path, serialized);
+        }
+
+
+        /// <summary>
+        /// Saves this puzzle asynchronously at <paramref name="path"/>. Specifically, the expected solution and dimension are stored.
+        /// See also <seealso cref="File.WriteAllBytesAsync(string, byte[], CancellationToken)"/>.
+        /// </summary>
+        /// <param name="path">The path to save the puzzle to</param>
+        public async Task SavePuzzleAsync(string path)
+        {
+            byte[] serialized = Serialize();
+            // No SerializeAsync method as puzzles are usually small in terms of bytes.
+            // This is also the reason that there are no async binarywriter/readers available in .NET
+
+            await File.WriteAllBytesAsync(path, serialized);
+        }
+
+        /// <summary>
+        /// Loads the puzzle located at <paramref name="path"/>
+        /// </summary>
+        /// <param name="path">Path of the puzzle to load</param>
+        /// <returns>A PuzzleDefinition of the requested puzzle, if available</returns>
+        public static PuzzleDefinition LoadPuzzle(string path)
+        {
+            byte[] serializedPuzzle = File.ReadAllBytes(path);
+            return Deserialize(serializedPuzzle);
+        }
+
+
+        /// <summary>
+        /// Loads the puzzle located at <paramref name="path"/> asynchronously
+        /// </summary>
+        /// <param name="path">Path of the puzzle to load</param>
+        /// <returns>A PuzzleDefinition of the requested puzzle, if available</returns>
+        public static async Task<PuzzleDefinition> LoadPuzzleAsync(string path)
+        {
+            byte[] serializedPuzzle = await File.ReadAllBytesAsync(path);
+            return Deserialize(serializedPuzzle);
+        }
+    }
+}
---
+++ tests/Picross.Game.Tests/PuzzleDefinitionTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picross.Game.Tests
+{
+    public class PuzzleDefinitionTests
+    {
+        // Basic solution with corresponding puzzle def
+        private static readonly bool[] sol = [
+            false, true, false, false, false, true, true, true,
+            true, false, true, false, false, false, true];
+        private readonly PuzzleDefinition puzzle = new(sol.Length, 1, sol);
+
+        [Fact]
+        public void TestConvertSolutionToBytesSingleByte()
+        {
+            PuzzleDefinition p = new(4, 1, [true, true, false, true]);
+            // 11010000 base 2 = 208 base 10
+
+            byte[] bytes = p.ConvertSolutionToBytes();
+            Assert.Single(bytes);
+            Assert.Equal(208, bytes[0]);
+        }
+
+        [Fact]
+        public void TestConvertSolutionToBytesMultipleBytes()
+        {
+
+            PuzzleDefinition p = new(sol.Length, 1, sol);
+            // 01000111 , 10100010 base 2 = 71 162 base 10
+
+            byte[] bytes = p.ConvertSolutionToBytes();
+            Assert.Equal(2, bytes.Length);
+            Assert.Equal(71, bytes[0]);
+            Assert.Equal(162, bytes[1]);
+        }
+
+        [Fact]
+        public void TestSerialize()
+        {
+            byte[] serialized = puzzle.Serialize();
+            
+            // 3 * 32 bits for version, width, height (=3*4 bytes) and the bytes for the solution
+            Assert.Equal(3 * 4 + 2, serialized.Length);
+
+            // Version
+            Assert.Equal(PuzzleDefinition.Version, BitConverter.ToInt32(serialized.AsSpan()[0..4]));
+
+            // Width & height
+            Assert.Equal(sol.Length, BitConverter.ToInt32(serialized.AsSpan()[4..8]));
+            Assert.Equal(1, BitConverter.ToInt32(serialized.AsSpan()[8..12]));
+
+            // Serialized solution, tested before
+            Assert.Equal(71, serialized[12]);
+            Assert.Equal(162, serialized[13]);
+        }
+
+        [Fact]
+        public void TestConvertBytesToSolution()
+        {
+            byte[] bytes = [71, 162];
+
+            bool[] determinedSolution = PuzzleDefinition.ConvertBytesToSolution(bytes, sol.Length, 1);
+
+            Assert.Equal(sol.Length, determinedSolution.Length);
+            Assert.Equal(sol, determinedSolution);
+        }
+
+        [Fact]
+        public void TestDeserialize()
+        {
+            PuzzleDefinition receivedDefinition = PuzzleDefinition.Deserialize(puzzle.Serialize());
+
+            Assert.Equal(puzzle.Width, receivedDefinition.Width);
+            Assert.Equal(puzzle.Height, receivedDefinition.Height);
+            Assert.Equal(sol, receivedDefinition.Solution);
+        }
+    }
+}
