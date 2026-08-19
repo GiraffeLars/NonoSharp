dcc9f01d44536b530de509b041ab7c7747345b69
GiraffeLars <203875086+GiraffeLars@users.noreply.github.com>
2026-08-19 14:32:20 +0200

    Add failing task to test github badge

--- tests/Picross.Game.Tests/GameAPIEventsTests.cs
+++ tests/Picross.Game.Tests/GameAPIEventsTests.cs
@@ -8,6 +8,12 @@ namespace Picross.Game.Tests
     public class GameAPIEventsTests
     {
         [Fact]
+        public void TestFail()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
         public async Task TestCellStateChangedEvent()
         {
             GameAPI api = new(new Grid(1, 1));
