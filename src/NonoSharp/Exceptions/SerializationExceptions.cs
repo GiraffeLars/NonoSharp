1c88c066ea4c1de07297e2ed062fbaa342c0a86b
GiraffeLars <203875086+GiraffeLars@users.noreply.github.com>
2026-08-25 19:27:48 +0200

    Made constructors public with documentation and remove Serializable

--- src/NonoSharp/Exceptions/SerializationExceptions.cs
+++ src/NonoSharp/Exceptions/SerializationExceptions.cs
@@ -7,17 +7,29 @@ namespace NonoSharp.Exceptions
     /// <summary>
     /// Raised when a given file was not of the expected format and is thus incompatible.
     /// </summary>
-    [Serializable]
     public class InvalidFileFormatException : Exception
     {
-        internal InvalidFileFormatException()
+        /// <summary>
+        /// Creates a new InvalidFileFormatException
+        /// </summary>
+        public InvalidFileFormatException()
         { }
 
-        internal InvalidFileFormatException(string? message)
+        /// <summary>
+        /// Creates a new InvalidFileFormatException with an error message <paramref name="message"/>.
+        /// </summary>
+        /// <param name="message">Error message to add</param>
+        public InvalidFileFormatException(string? message)
             : base(message)
         { }
 
-        internal InvalidFileFormatException(string? message, Exception? innerException)
+        /// <summary>
+        /// Creates a new InvalidFileFormatException together with an error message <paramref name="message"/> and an
+        /// inner exception <paramref name="innerException"/>.
+        /// </summary>
+        /// <param name="message">Error message to add</param>
+        /// <param name="innerException">Inner exception to add</param>
+        public InvalidFileFormatException(string? message, Exception? innerException)
             : base(message, innerException)
         { }
     }
@@ -25,17 +37,29 @@ namespace NonoSharp.Exceptions
     /// <summary>
     /// Thrown when deserialization of a puzzle fails.
     /// </summary>
-    [Serializable]
     public class PuzzleDeserializationFailedException : Exception
     {
-        internal PuzzleDeserializationFailedException()
+        /// <summary>
+        /// Creates a new PuzzleDeserializationFailedException
+        /// </summary>
+        public PuzzleDeserializationFailedException()
         { }
 
-        internal PuzzleDeserializationFailedException(string? message)
+        /// <summary>
+        /// Creates a new PuzzleDeserializationFailedException with an error message <paramref name="message"/>.
+        /// </summary>
+        /// <param name="message">Error message to add</param>
+        public PuzzleDeserializationFailedException(string? message)
             : base(message)
         { }
 
-        internal PuzzleDeserializationFailedException(string? message, Exception? innerException)
+        /// <summary>
+        /// Creates a new PuzzleDeserializationFailedException together with an error message <paramref name="message"/> and an
+        /// inner exception <paramref name="innerException"/>.
+        /// </summary>
+        /// <param name="message">Error message to add</param>
+        /// <param name="innerException">Inner exception to add</param>
+        public PuzzleDeserializationFailedException(string? message, Exception? innerException)
             : base(message, innerException)
         { }
     }
@@ -43,17 +67,29 @@ namespace NonoSharp.Exceptions
     /// <summary>
     /// Thrown when serialization of a puzzle fails.
     /// </summary>
-    [Serializable]
     public class PuzzleSerializationFailedException : Exception
     {
-        internal PuzzleSerializationFailedException()
+        /// <summary>
+        /// Creates a new PuzzleSerializationFailedException
+        /// </summary>
+        public PuzzleSerializationFailedException()
         { }
 
-        internal PuzzleSerializationFailedException(string? message)
+        /// <summary>
+        /// Creates a new PuzzleSerializationFailedException with an error message <paramref name="message"/>.
+        /// </summary>
+        /// <param name="message">Error message to add</param>
+        public PuzzleSerializationFailedException(string? message)
             : base(message)
         { }
 
-        internal PuzzleSerializationFailedException(string? message, Exception? innerException)
+        /// <summary>
+        /// Creates a new PuzzleSerializationFailedException together with an error message <paramref name="message"/> and an
+        /// inner exception <paramref name="innerException"/>.
+        /// </summary>
+        /// <param name="message">Error message to add</param>
+        /// <param name="innerException">Inner exception to add</param>
+        public PuzzleSerializationFailedException(string? message, Exception? innerException)
             : base(message, innerException)
         { }
     }
@@ -61,17 +97,29 @@ namespace NonoSharp.Exceptions
     /// <summary>
     /// Thrown when a puzzle could not be saved to a file.
     /// </summary>
-    [Serializable]
     public class PuzzleSavingFailedException : Exception
     {
-        internal PuzzleSavingFailedException()
+        /// <summary>
+        /// Creates a new PuzzleSavingFailedException
+        /// </summary>
+        public PuzzleSavingFailedException()
         { }
 
-        internal PuzzleSavingFailedException(string? message)
+        /// <summary>
+        /// Creates a new PuzzleSavingFailedException with an error message <paramref name="message"/>.
+        /// </summary>
+        /// <param name="message">Error message to add</param>
+        public PuzzleSavingFailedException(string? message)
             : base(message)
         { }
 
-        internal PuzzleSavingFailedException(string? message, Exception? innerException)
+        /// <summary>
+        /// Creates a new PuzzleSavingFailedException together with an error message <paramref name="message"/> and an
+        /// inner exception <paramref name="innerException"/>.
+        /// </summary>
+        /// <param name="message">Error message to add</param>
+        /// <param name="innerException">Inner exception to add</param>
+        public PuzzleSavingFailedException(string? message, Exception? innerException)
             : base(message, innerException)
         { }
     }
@@ -79,17 +127,29 @@ namespace NonoSharp.Exceptions
     /// <summary>
     /// Thrown when the puzzle could not be loaded from e.g. a file.
     /// </summary>
-    [Serializable]
     public class PuzzleLoadingFailedException : Exception
     {
-        internal PuzzleLoadingFailedException()
+        /// <summary>
+        /// Creates a new PuzzleLoadingFailedException
+        /// </summary>
+        public PuzzleLoadingFailedException()
         { }
 
-        internal PuzzleLoadingFailedException(string? message)
+        /// <summary>
+        /// Creates a new PuzzleLoadingFailedException with an error message <paramref name="message"/>.
+        /// </summary>
+        /// <param name="message">Error message to add</param>
+        public PuzzleLoadingFailedException(string? message)
             : base(message)
         { }
 
-        internal PuzzleLoadingFailedException(string? message, Exception? innerException)
+        /// <summary>
+        /// Creates a new PuzzleLoadingFailedException together with an error message <paramref name="message"/> and an
+        /// inner exception <paramref name="innerException"/>.
+        /// </summary>
+        /// <param name="message">Error message to add</param>
+        /// <param name="innerException">Inner exception to add</param>
+        public PuzzleLoadingFailedException(string? message, Exception? innerException)
             : base(message, innerException)
         { }
     }
