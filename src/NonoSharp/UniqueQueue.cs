633c15804d3d3282ba7915f94c9b97f5aa7c5b77
GiraffeLars <203875086+GiraffeLars@users.noreply.github.com>
2026-08-31 14:04:31 +0200

    Implement UniqueQueue

---
+++ src/NonoSharp/UniqueQueue.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NonoSharp
+{
+    /// <summary>
+    /// A queue where each element can only appear once in the queue. Works on a FIFO basis.
+    /// Functions like <see cref="Queue"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements for the queue</typeparam>
+    internal class UniqueQueue<T> : IEnumerable<T>
+    {
+        private readonly Queue<T> queue;
+        private readonly HashSet<T> enqueued;
+
+        /// <summary>
+        /// The total number of elements currently in the queue
+        /// </summary>
+        public int Count { get { return queue.Count; } }
+
+        public UniqueQueue()
+        {
+            queue = [];
+            enqueued = [];
+        }
+
+        /// <summary>
+        /// Enqueues <paramref name="element"/>. If <paramref name="element"/> is already in the queue, nothing changes.
+        /// </summary>
+        /// <param name="element">The element to enqueue</param>
+        public void Enqueue(T element)
+        {
+            if (!enqueued.Contains(element))
+            {
+                queue.Enqueue(element);
+                enqueued.Add(element);
+            }
+
+        }
+
+        /// <summary>
+        /// Removes the first element from the queue.
+        /// </summary>
+        /// <returns>The dequeued element</returns>
+        public T Dequeue()
+        {
+            T element = queue.Dequeue();
+            enqueued.Remove(element);
+            return element;
+        }
+
+        /// <summary>
+        /// Returns the first element of the queue without removing it.
+        /// </summary>
+        /// <returns>The first element of the queue, if any</returns>
+        /// <exception cref="InvalidOperationException">Thrown when the queue is empty</exception>
+        public T Peek()
+        {
+            return queue.Peek();
+        }
+
+        /// <summary>
+        /// Determines whether <paramref name="element"/> is currently in the queue
+        /// </summary>
+        /// <param name="element">The element to check for</param>
+        /// <returns>True if the element is in the queue, false otherwise</returns>
+        public bool Contains(T element)
+        {
+            return enqueued.Contains(element);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return queue.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
